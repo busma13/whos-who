4daf9e9d49ef3575092e8e2d4896f72e5d33f037
busma13 <peterluitjens@gmail.com>
2023-06-18 20:42:53 -0700

    Add more types. Update how artists and tracks are chosen.

--- src/types/Home.ts
+++ src/types/Home.ts
@@ -2,3 +2,26 @@ export interface SpotifyToken {
   value: string,
   expiration: number
 }
+
+export interface Artist {
+  name: string
+}
+
+export interface Track {
+  name: string,
+  album: {images: Image[]},
+  artists: [Artist],
+  popularity: number,
+  preview_url: string
+}
+
+export interface Image {
+    height: number,
+    url: string,
+    width: number
+}
+
+export interface ArtistGuessData {
+  artist: Artist;
+  image: Image
+}[]
